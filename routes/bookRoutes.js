2051dc2b8fc5520e5ef367eec871038ebd0e9bbe
shashikant yadav <shashikant.sy126@gmail.com>
2023-11-09 02:44:08 +0530

    added postman collection

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,7 +8,7 @@ bookRouter.use(express.urlencoded({ extended: true }));
 bookRouter.post("/addbook",Role([1]),bookController.addBook);
 bookRouter.get("/getbooklist",Role([1]),bookController.getBookList);
 bookRouter.get("/getbookdetail/:id",Role([1]),bookController.getBookDetail);
-bookRouter.get("/deletebook/:id",Role([1]),bookController.deleteBook);
+bookRouter.delete("/deletebook/:id",Role([1]),bookController.deleteBook);
 bookRouter.put("/updatebook",Role([1]),bookController.updateBook);
 
 
